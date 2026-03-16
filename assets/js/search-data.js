09e757c075a2020234bc28ab33440734a83b64a0
Jii111 <90593855+Jii111@users.noreply.github.com>
2026-03-16 18:37:36 +0000

    Deploying to gh-pages from @ Jii111/jii111.github.io@ccce83a70afd51b58407231faef65db2ddb24f6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -153,9 +153,9 @@ ninja.data = [{
           section: "News",},{id: "news-admission-to-the-m-s-program-in-statistics-yonsei-university",
           title: 'Admission to the M.S. Program in Statistics, Yonsei University',
           description: "",
-          section: "News",},{id: "projects-한국어-ai-챗봇의-비용-효율화-및-질의-대응-성능-고도화-연구",
-          title: '한국어 AI 챗봇의 비용 효율화 및 질의 대응 성능 고도화 연구',
-          description: "실제 서비스 환경에서 비용 효율성과 도메인 적응성을 갖춘 한국어 질의 대응 프레임워크 구축",
+          section: "News",},{id: "projects-cost-efficient-and-domain-adaptive-korean-ai-chatbot-development",
+          title: 'Cost-Efficient and Domain-Adaptive Korean AI Chatbot Development',
+          description: "한국어 AI 챗봇의 비용 효율화 및 질의 대응 성능 고도화 연구\n 실제 서비스 환경에서 비용 효율성과 도메인 적응성을 갖춘 한국어 질의 대응 프레임워크 구축",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-clustering-for-customer-segmentation-and-identification-of-significant-survey-indicators",
@@ -163,6 +163,11 @@ ninja.data = [{
           description: "연세데이터사이언스연구소",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-behavioral-log-analysis-of-financial-app-users",
+          title: 'Behavioral Log Analysis of Financial App Users',
+          description: "금융 어플 사용자 행동 데이터를 이용한 대출 신청 예측 및 고객 군집 분석)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
